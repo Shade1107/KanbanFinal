9c3f07e6d9516a01f07aba3301ba16845c2d942c
SiThuwin2002 <sithu1328@gmail.com>
2024-04-02 13:23:05 +0630

    back

--- pages/memberlist.php
+++ pages/memberlist.php
@@ -25,6 +25,8 @@
 <body>
 
 <section class="column-container container" id="container">
+
+
     <div class="task-column item" draggable="true" id="backlog" style="width:100%">
         <h3>✔ Member list ✔</h3>
         <hr class="custom-hr" />
@@ -54,7 +56,19 @@
                         <?php endforeach ?>
                     </tbody>
             </table>
+
+         <div class="col-md-6">
+            <a href="javascript:history.back()"><svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="#79305a" class="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
+        <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1"/>
+        </svg></a>
+        </div>
+        <br>
+
+        
+
     </div>
+
+
 </section>
 
 <script>
