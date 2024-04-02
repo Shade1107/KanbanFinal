5d3fbb93b994a21044c2fc1523a16345653a52bd
someone <myatthuukha032@gmail.com>
2024-04-02 21:04:29 +0630

    pf all done

--- header_footer/header.php
+++ header_footer/header.php
@@ -2,10 +2,12 @@
 session_start(); 
 $isMember = $isMember??'';
 $isAdmin = $isAdmin??'';
-
+require_once("../Repositories/UserRepository.php");
 if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
 {
   $userID = $_SESSION['user_id'];
+  $userRepo = new UserRepository(DatabaseConnection::getInstance());
+  $user = $userRepo->find($userID);
 }
 $isAdminMemberFromPJwebpage = $isAdminMemberFromPJwebpage??'';
 // if ($isMember) {
@@ -32,7 +34,7 @@ $isAdminMemberFromPJwebpage = $isAdminMemberFromPJwebpage??'';
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
      <link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="css/style.css" />
 
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
 
@@ -56,13 +58,11 @@ $isAdminMemberFromPJwebpage = $isAdminMemberFromPJwebpage??'';
           <a href="../Functions4Kanban/signout.php" class="btn  mt-3 ">LogOut</a>
           <div class="d-flex Profilecircle mr-3">
                 <a href="viewprofile.php?id=<?= $userID ?>" class="circle-container">
+                <?php
+               $imagePath = (isset($user->img) && !empty($user->img)) ? "../image/".$user->img : "../image/default.jpg";
+                ?>
+                <img src="<?= $imagePath ?>" id="photoPreview" class="avatar img-circle img-thumbnail" alt="avatar">
 
-                <?php 
-                    if ($isAdminMemberFromPJwebpage) { ?>
-                     <img src="../image/p2.jpg">
-                    <?php } ?>
-
-                    <img src="../image/p2.jpg">
                 </a>
                 
           </div>
