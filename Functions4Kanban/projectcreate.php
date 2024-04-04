ae33e8e31909199db31872554613d729691f8920
PPPaing <cybirious@gmail.com>
2024-04-04 13:53:01 +0630

    pppaing's changes

--- Functions4Kanban/projectcreate.php
+++ Functions4Kanban/projectcreate.php
@@ -6,30 +6,25 @@
 <?php
     
     $projectRepo = new ProjectRepository(DatabaseConnection::getInstance());
-
-        $admin_id = $_POST['admin_id'];
-        $name = $_POST["projectName"];
-        $membersArray = $_POST["members"];
-        $description = $_POST["Description"];
-        $detail_descrip = $_POST["Detail_Description"];
-        $create_date = $_POST["createDate"];
-        $due_date = $_POST["dueDate"];
+    
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $admin_id = DatabaseConnection::getInstance()->real_escape_string($_POST['admin_id']);
+        $name = DatabaseConnection::getInstance()->real_escape_string($_POST["projectName"]);
+        $users_id = $_POST["members"];
+        $description = DatabaseConnection::getInstance()->real_escape_string($_POST["Description"]);
+        $detail_descrip = DatabaseConnection::getInstance()->real_escape_string($_POST["Detail_Description"]);
+        $create_date = DatabaseConnection::getInstance()->real_escape_string($_POST["createDate"]);
+        $due_date = DatabaseConnection::getInstance()->real_escape_string($_POST["dueDate"]);
         
-        echo $admin_id."<br>";
-        echo $name."<br>";
-        echo $description."<br>";
-        echo $detail_descrip."<br>";
-        echo $create_date."<br>";
-        echo $due_date."<br>";
-        print_r($membersArray);
-        $result = $projectRepo->create($admin_id, $name, $description, $detail_descrip, $create_date, $due_date, $membersArray);
-        // if ($result) {
-        //   header("Location: ../pages/addProjectAdmin.php");
-        //   exit();
-        // } else {
-        // echo "Error inserting project.";
-        //} 
-    // else {
-    //     echo "one or more require";
-    //}
+        $result = $projectRepo->create($admin_id, $name, $description, $detail_descrip, $create_date, $due_date, $users_id);
+        if ($result) {
+          header("Location: ../pages/addProjectAdmin.php");
+          exit();
+        } else {
+            $error_message = "Error inserting task.";
+        } 
+    }
+    else{
+        $error_message = "One or more required fields are missing.";
+    }
 ?>
