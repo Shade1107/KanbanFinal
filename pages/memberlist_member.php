4b7630757e8d7bc7f9034634760be2d560c10264
SiThuwin2002 <sithu1328@gmail.com>
2024-04-05 18:42:34 +0630

    up

--- pages/memberlist_member.php
+++ pages/memberlist_member.php
@@ -34,7 +34,6 @@
                         <th>Picture</th>
                         <th>Name</th>
                         <th>Email</th>
-                        <th onclick="togglePassword()">Password</th>
                         <th>Gender</th>
                         <th>Role</th>
                     </tr>
@@ -45,8 +44,7 @@
                                 <td ><?= $m->id ?></td>
                                 <td><img src="../image/<?=$m->img?>" style="max-width: 50px; max-height: 50px;"></td>
                                 <td><?= $m->name ?></td>
-                                <td><?= $m->email ?></td>  
-                                <td data-password="<?= $m->password ?>">***</td>   
+                                <td><?= $m->email ?></td>    
                                 <td><?= $m->getGender()->name ?></td>     
                                 <td><?= $m->getRole()->name ?></td>
                             </td>      
@@ -69,23 +67,7 @@
 
 </section>
 
-<script>
-        function togglePassword() {
-            // Get all the password cells
-            var passwordCells = document.querySelectorAll('td[data-password]');
 
-            // Toggle the visibility of the password value for each password cell
-            passwordCells.forEach(function(cell) {
-                if (cell.textContent === '***') {
-                    // Get the actual password value from the "data-password" attribute
-                    var password = cell.getAttribute('data-password');
-                    cell.textContent = password; // Display the actual password value
-                } else {
-                    cell.textContent = '***'; // Hide the password value and display asterisks
-                }
-            });
-        }
-        </script>
 </body>
 </html>
  
