b3ee1698997b6423cef69451b31d89f0d8b99723
SiThuwin2002 <sithu1328@gmail.com>
2024-04-07 21:59:03 +0630

    comfirm delete

--- pages/delete_memberlist.php
+++ pages/delete_memberlist.php
@@ -3,11 +3,20 @@
     require_once("../Repositories/UserRepository.php");
     require_once("../Database/DatabaseConnection.php");
 ?>
+
+
 <?php
-    $userRepo = new UserRepository(DatabaseConnection::getInstance());
-    $result = $userRepo->delete($_GET['id']);
-    if($result){
-        header("Location: memberlist.php");
-    }
-?>
+    
+        $userRepo = new UserRepository(DatabaseConnection::getInstance());
+        $result = $userRepo->delete($_GET['id']);
+        if($result){
+            header("Location: memberlist.php");
+        }  
+    
+   
+?> 
+
+
+
+
 
